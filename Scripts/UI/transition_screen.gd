b7c4ef1142109e7f66a094dbf68a64166a3f6651
Charles Doucet <charles.douc@gmail.com>
2024-09-13 15:54:28 -0400

    fix: typos in dissolve animations

--- Scripts/UI/transition_screen.gd
+++ Scripts/UI/transition_screen.gd
@@ -1,13 +1,13 @@
 extends CanvasLayer
 
 @export var transition_color: Color = Color.WHITE
-@export var animation_type := AnimationType.DISOLVE_COLOR
+@export var animation_type := AnimationType.DISSOLVE_COLOR
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 enum AnimationType {
-	DISOLVE_COLOR,
-	DISOLVE_CONTENT,
+	DISSOLVE_COLOR,
+	DISSOLVE_CONTENT,
 	SWIPE_COLOR,
 	SWIPE_CONTENT,
 }
@@ -19,10 +19,10 @@ func play_transition_screen() -> void:
 	animated_sprite_2d.modulate = transition_color;
 	
 	match animation_type:
-		AnimationType.DISOLVE_COLOR:
-			animated_sprite_2d.play("disolve_color")
-		AnimationType.DISOLVE_CONTENT:
-			animated_sprite_2d.play("disolve_content")
+		AnimationType.DISSOLVE_COLOR:
+			animated_sprite_2d.play("dissolve_color")
+		AnimationType.DISSOLVE_CONTENT:
+			animated_sprite_2d.play("dissolve_content")
 		AnimationType.SWIPE_COLOR:
 			animated_sprite_2d.play("swipe_color")
 		AnimationType.SWIPE_CONTENT:
