521b8ef6a6c681e9cf6c0f0007eb8f524fa8b95e
Charles Doucet <charles.douc@gmail.com>
2024-09-14 19:06:08 -0400

    wip: top and down view controls

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var player_control_type := PlayerControlTypes.SIDE_VIEW
 @export var movement_speed := 100.0
 @export_subgroup("Side View")
 @export var gravity := 300.0
@@ -7,15 +8,23 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+enum PlayerControlTypes
+{
+	SIDE_VIEW,
+	TOP_VIEW,
+	DOWN_VIEW
+}
 
 
 func _physics_process(delta: float) -> void:
-	side_movement(delta)
+	match player_control_type:
+		PlayerControlTypes.SIDE_VIEW:
+			side_movement(delta)
+		PlayerControlTypes.TOP_VIEW:
+			top_view_movement(delta)
 
 
 func side_movement(delta) -> void:
-	var direction = 0
-	
 	if !is_on_floor():
 		velocity.y += gravity * delta
 		if velocity.y > 400.0:
@@ -24,10 +33,23 @@ func side_movement(delta) -> void:
 	if Input.is_action_just_pressed("up_dPad") && is_on_floor():
 		velocity.y = -jump_force
 	
-	direction = Input.get_axis("left_dPad","right_dPad")
+	var direction = Input.get_axis("left_dPad","right_dPad")
 	
 	if direction != 0:
 		animated_sprite_2d.flip_h = (direction == -1)
 	
 	velocity.x = direction * movement_speed
 	move_and_slide()
+ 
+
+func top_view_movement(delta) -> void:
+	var horizontal_direction = Input.get_axis("left_dPad", "right_dPad")
+	var vertical_direction = Input.get_axis("ui_up", "down_dPad")
+	
+	velocity.x = horizontal_direction * movement_speed
+	velocity.y = vertical_direction * movement_speed
+	
+	move_and_slide()
+	
+	
+	
