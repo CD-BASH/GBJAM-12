4644396e1ecdc6ed6d80166423d276d2fb6f06cb
flix55 <flixgagne@gmail.com>
2024-09-14 02:59:46 -0400

    Shader gameboy Test

---
+++ Scenes/sub_viewport_container.gd
@@ -0,0 +1,13 @@
+extends SubViewportContainer
+var time : float
+@export var transitionAmmount : float = 2 
+@export var speed : float = 0.2 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#time += delta
+	#stretch_shrink = round(abs(sin(time * speed) * transitionAmmount))
