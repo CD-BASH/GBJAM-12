4c706f79ade35ea4946ed2325ab82d25653738c9
Charles Doucet <charles.douc@gmail.com>
2024-09-15 11:44:25 -0400

    feat: down view controls with new collision shapes

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -5,8 +5,15 @@ extends CharacterBody2D
 @export_subgroup("Side View")
 @export var gravity := 300.0
 @export var jump_force := 150.0
+@export var side_collision_shape: RectangleShape2D
+@export_subgroup("Top View")
+@export var top_collision_shape: RectangleShape2D
+@export_subgroup("Down View")
+@export var down_collision_shape: RectangleShape2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
 
 enum PlayerControlTypes
 {
@@ -20,8 +27,13 @@ func _physics_process(delta: float) -> void:
 	match player_control_type:
 		PlayerControlTypes.SIDE_VIEW:
 			side_movement(delta)
+			collision_shape_2d.shape = side_collision_shape
 		PlayerControlTypes.TOP_VIEW:
-			top_view_movement(delta)
+			top_down_view_movement(delta, true)
+			collision_shape_2d.shape = top_collision_shape
+		PlayerControlTypes.DOWN_VIEW:
+			top_down_view_movement(delta, false)
+			collision_shape_2d.shape = down_collision_shape
 
 
 func side_movement(delta) -> void:
@@ -44,11 +56,14 @@ func side_movement(delta) -> void:
 	move_and_slide()
  
 
-func top_view_movement(delta) -> void:
-	animated_sprite_2d.play("top_idle")
+func top_down_view_movement(delta, top_view: bool) -> void:
+	if top_view:
+		animated_sprite_2d.play("top_idle")
+	else:
+		animated_sprite_2d.play("down_idle")
 	
 	var horizontal_direction = Input.get_axis("left_dPad", "right_dPad")
-	var vertical_direction = Input.get_axis("ui_up", "down_dPad")
+	var vertical_direction = Input.get_axis("up_dPad", "down_dPad")
 	
 	velocity.x = horizontal_direction * movement_speed
 	velocity.y = vertical_direction * movement_speed
