40eff7af0a763627d3a7dddc5cd58014b24a7e33
flix55 <flixgagne@gmail.com>
2024-09-16 21:19:27 -0400

    feat : Made postprocess gamecube filter/pixellisation effect
    Assets made for shader
    WIP : there's a 3dtest scene with all of the experiment

--- Scripts/Gameplay/sub_viewport_container.gd
+++ Scripts/Gameplay/sub_viewport_container.gd
@@ -8,6 +8,6 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#time += delta
-	#stretch_shrink = round(abs(sin(time * speed) * transitionAmmount))
+func _process(delta):
+	time += delta
+	stretch_shrink = round(abs(sin(time * speed) * transitionAmmount))
