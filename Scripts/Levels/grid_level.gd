20c2483205929fbf8a7e0082682aa9502283b491
Charles Doucet <charles.douc@gmail.com>
2024-09-17 18:54:36 -0400

    feat: [38] Safe areas to hide the player
    Closes #38

--- Scripts/Levels/grid_level.gd
+++ Scripts/Levels/grid_level.gd
@@ -12,10 +12,11 @@ var cell_size := 16.0
 var player_cell_coordinate: Vector2
 var player_depth: float
 var player_altitude: float
-var player_ground_level: float
 var current_view: ViewMode
-var player_index_original;
-var player_index_behind_platforms := -40.0;
+var player_index_original
+var player_index_behind_platforms := -30.0
+var player_in_depth_safe_zone := false
+var player_in_altitude_safe_zone := false
 
 enum ViewMode
 {
@@ -31,8 +32,6 @@ func _ready() -> void:
 	player_depth = ground_level
 	player_index_original = player.z_index
 	set_new_view(starting_view)
-	
-
 
 func _process(delta: float) -> void:
 	get_player_position_on_grid()
@@ -41,6 +40,9 @@ func _process(delta: float) -> void:
 		set_new_view(ViewMode.TOP_VIEW)
 	if Input.is_action_just_pressed("b_btn"):
 		set_new_view(ViewMode.SIDE_VIEW)
+		
+	if (player_in_depth_safe_zone && player_in_altitude_safe_zone):
+		player_is_safe = true
 
 
 func set_new_view(view: ViewMode) -> void:
@@ -57,12 +59,12 @@ func set_new_view(view: ViewMode) -> void:
 	current_view = view
 
 
+#region Player Positions
 func new_player_position(y_cell_position: float) -> void:
 	player.global_position = Vector2(
 		player_cell_coordinate.x * cell_size, 
 		y_cell_position * cell_size
 	)
-
 
 func get_player_position_on_grid():
 	player_cell_coordinate = Vector2(
@@ -72,3 +74,18 @@ func get_player_position_on_grid():
 	match current_view:
 		ViewMode.TOP_VIEW:
 			player_depth = player_cell_coordinate.y
+#endregion
+
+
+#region Safe Zone Evaluations
+func track_safe_zone_depth(state: bool) -> void:
+	if current_view != ViewMode.TOP_VIEW: return
+	player_in_depth_safe_zone = state
+	player.z_index = player_index_behind_platforms if state else player_index_original
+	#print("Depth Safe Zone: " + str(state))
+
+func track_safe_zone_altitude(state: bool) -> void:
+	if current_view != ViewMode.SIDE_VIEW: return
+	player_in_altitude_safe_zone = state
+	#print("Altitude Safe Zone: " + str(state))
+#endregion
