d892ed96a7e9dca82936e8772fa1f3086c589acc
Goldorak5 <114761586+Goldorak5@users.noreply.github.com>
2024-09-17 22:48:58 -0400

    turret dans les 4 sens

--- Scripts/Enemies/enemie_turret.gd
+++ Scripts/Enemies/enemie_turret.gd
@@ -1,18 +1,54 @@
 extends Node2D
 
 @onready var left_raycast = $left_raycast
-@onready var right_raycast = $"right-raycast"
-@onready var top_raycast = $top_raycast
+@onready var right_raycast = $right_raycast
+@onready var up_raycast = $up_raycast
 @onready var down_raycast = $down_raycast
+@onready var seen_sound = $seen_sound
+#@onready var timer = $Timer
 
+#variables in the inspector choose the direction that can see the player
+@export_group("Raycast direction")
+@export var right = false
+@export var left = false
+@export var down = false
+@export var up = false
+
+#if we want the player to be seen only once
+var player_seen = false
 
 func _ready():
-	pass 
-
+	chose_raycast_direction()
 
 func _process(delta):
-	pass
+	if (left_raycast.is_colliding() || right_raycast.is_colliding() || up_raycast.is_colliding() || down_raycast.is_colliding()) && !player_seen:
+		#debbugging feature can be erase 
+		print("I SEE YOU!")
+		
+		#play sound when seen
+		seen_sound.play()
+		#variable to be seen only once
+		player_seen = true
+		
+		#code here when the player is seen
+		
+		
+		
+		#add the timer to be seen multiple times
+		#timer.wait_time = 1
+		#timer.start()
+		
 
-#funtion to implement in level script
-func chose_raycast_direction(direction: string):
+func chose_raycast_direction() -> void:
+	if right:
+		right_raycast.enabled = true
+	if left:
+		left_raycast.enabled = true
+	if down:
+		down_raycast.enabled = true
+	if up:
+		up_raycast.enabled = true
 	
+
+#func _on_timer_timeout():
+#	player_seen = false
