1debe5673132c8eb11ced76bde5e73a7dde21176
Charles Doucet <charles.douc@gmail.com>
2024-09-21 15:09:56 -0400

    fix: minor fixes

--- Scripts/Levels/grid_view.gd
+++ Scripts/Levels/grid_view.gd
@@ -5,9 +5,7 @@ extends Node2D
 
 func activate_view(state: bool):
 	visible = state
-	print(tilemaps)
 	for i in tilemaps.size():
 		tilemaps[i].enabled = state
-		print("Hi")
 	background.visible = state
 	self.process_mode = Node.PROCESS_MODE_INHERIT if state else Node.PROCESS_MODE_DISABLED
