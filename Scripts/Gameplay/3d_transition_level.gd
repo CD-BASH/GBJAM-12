b244701e1c3e9fc62484ac7a683a17ca4003bc1c
flix55 <flixgagne@gmail.com>
2024-09-22 02:11:39 -0400

    setting up stop buffer when switching

--- Scripts/Gameplay/3d_transition_level.gd
+++ Scripts/Gameplay/3d_transition_level.gd
@@ -2,21 +2,47 @@ extends Node3D
 
 @onready var front: MeshInstance3D = $Marker3D/Front
 @onready var top: MeshInstance3D = $Marker3D/Top
-@export var subviewport_front : ViewportTexture
-@export var subviewport_top : ViewportTexture
+@onready var mats : Material
+
+@export_subgroup("front")
+@export var subviewport_texture_front : ViewportTexture
+@export_subgroup("Top")
+@export var subviewport_texture_top : ViewportTexture
+
+@export var freeze_viewport : SubViewport
+@export var gameplay_viewport : SubViewport
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	front.mesh.material.set_shader_parameter("render_target",subviewport_front)
-	top.mesh.material.set_shader_parameter("render_target",subviewport_top)
-	
+	front.mesh.material.set_shader_parameter("render_target",subviewport_texture_front)
+	top.mesh.material.set_shader_parameter("render_target",subviewport_texture_top)
+	## try that latter
 	##SetMaterial(front.mesh.material, subviewport_front)
 	##SetMaterial(top.mesh.material, subviewport_top)
 
 func _process(delta):
+	"""
+	if Input.is_action_just_pressed("a_btn"):
+		subviewport_front.render_target_update_mode = SubViewport.UPDATE_ONCE
+		subviewport_front.render_target_clear_mode = SubViewport.CLEAR_MODE_ONCE
+		subviewport_top.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+		subviewport_top.render_target_clear_mode = SubViewport.CLEAR_MODE_ALWAYS
+	
+	if Input.is_action_just_pressed("b_btn"):
+		subviewport_top.render_target_update_mode = SubViewport.UPDATE_ONCE
+		subviewport_top.render_target_clear_mode = SubViewport.CLEAR_MODE_ONCE
+		subviewport_front.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+		subviewport_front.render_target_clear_mode = SubViewport.CLEAR_MODE_ALWAYS
+	"""
 	pass
 
-func SetMaterial(mat :Material, textureViewport: ViewportTexture) -> void:
-	mat = ShaderMaterial.new()
-	mat.shader = preload("res://Shaders/Video.gdshader")
-	mat.set_shader_parameter("render_target",textureViewport)
+func SetViewPortes() -> void : 
+	
+	pass
+	
+
+func SetMaterial(textureViewport: ViewportTexture) -> void:
+	mats = ShaderMaterial.new()
+	mats.shader = preload("res://Shaders/Video.gdshader")
+	mats.set_shader_parameter("render_target",textureViewport)
+	
